90dc89bc8d779ba6c3b8509273556a5b6a5a7f13
Vivian Ramirez <vivianpaola2828@gmail.com>
2021-12-13 19:14:31 -0500

    Recorrer Array

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
-let nombrePlanetas = Array ('Tierra', 'Marte')
-let distancia= [5,3,'67']
-const tamaño = []
+let nombrePlanetas = Array ('Tierra', 'Marte', 'Jupiter')
+let distancia= [6,3,'67']
+const tamaño = [10,20,30]
 
 console.log(nombrePlanetas)
 console.log(distancia)
@@ -47,15 +47,33 @@ for (const planeta of nombrePlanetas){
 }
 */
 
+nombrePlanetas.forEach((value, index, array) =>{
+    console.log('planeta', index, value)
+});
 
-for (const key in distancia){
+
+
+/*nombrePlanetas.map((planetas,index)=>{
+    return(
+        console.log(`El planeta ${planetas} tiene tamaño de :${tamaño[index]}`)
+    )
+})*/
+
+
+let resultadoMap = nombrePlanetas.map((planetas, index) =>{
+    return(`El planeta ${planetas} tiene tamaño de :${tamaño[index]}`)
+})
+//es un array lo que muestra mientra que arriba imprime cada uno de los indices"el planeta tierra tien 10" ...
+console.log(resultadoMap)
+
+/*for (const key in distancia){
     if(distancia[key] =='5'){
         console.log('se encontro la distancia')
     }
     else{
         console.log('no se encontro la distancia')
     }
-}
+}*/
 
 
 
